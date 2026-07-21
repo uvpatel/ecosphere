97b5b9defbd434390fe0e875e0ac5c7262704ead
Urvil Patel <uvpatel7271@gmail.com>
2026-07-21 20:20:43 +0530

    Project setup done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { TooltipProvider } from "@/components/ui/tooltip"
+
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +30,11 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+      <TooltipProvider>
+      {children}
+      </TooltipProvider>
+      </body>
     </html>
   );
 }
